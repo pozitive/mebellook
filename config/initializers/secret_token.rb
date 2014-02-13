d07735fa8d24eae86ce163b20e3894b1c7107657
Dmitry Sukhikh <china@pozitive.biz>
2014-02-13 18:10:46 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mebellook::Application.config.secret_key_base = '08bd7ec1057fbba79921292680a8950b984d57def3cb52f43b3a0fd6224d3c97d1e417d24f1d54508f816bc5bdb62087c74c7156e691d4d4b8da11e1fcc14b92'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mebellook::Application.config.session_store :cookie_store, key: '_mebellook_session'
