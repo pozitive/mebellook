1a7c9490d39c9ce7d385cf86b8b5f651d50a23aa
Dmitry Sukhikh <china@pozitive.biz>
2014-02-25 19:52:00 +0700

    Created unit model

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,2 +1,4 @@
 class Unit < ActiveRecord::Base
+  mount_uploader :image, ImageUploader
+  validates :name, :image, presence: true
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -3,7 +3,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -32,9 +32,21 @@ class ImageUploader < CarrierWave::Uploader::Base
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  version :thumb do
+    process :resize_to_limit => [200, 200]
+  end
+
+  version :small do
+    process :resize_to_limit => [600, 600]
+  end
+
+  version :medium do
+    process :resize_to_limit => [1024, 1024]
+  end
+
+  version :large do
+    process :resize_to_limit => [1920, 1080]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
